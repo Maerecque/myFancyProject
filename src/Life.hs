463d63d7fb1ca5c159a1ac69c9b2a3cae810de02
Marc Zoomers <marczoomers@gmail.com>
2019-11-03 16:44:01 +0100

    Added Gosper glider gun

--- src/Life.hs
+++ src/Life.hs
@@ -46,7 +46,7 @@ formatGrid grid = do
       | x == maximum maxX = ['\n']
       | otherwise         = []
 
-    gridSize = [(0,0),(20,20)] -- size of the grid
+    gridSize = [(0,0),(50,20)] -- size of the grid
     maxX = gridRange fst
     maxY = gridRange snd
     gridRange f = [minGrid gridSize .. maxGrid gridSize] where
@@ -59,5 +59,22 @@ printGrid = putStrLn . formatGrid
 
 -- | main function to set everything in motion
 ui :: IO()
-ui = mapM_ printGrid (take 30 (iterate step cellCoordinates)) where -- ammount of evolutions is declared here
-    cellCoordinates = [(5, 0), (6, 1), (4, 2), (5, 2), (6, 2)] -- glider
+ui = mapM_ printGrid (take 31 (iterate step cellCoordinates)) where -- ammount of evolutions is declared here
+    -- cellCoordinates = [(5, 0), (6, 1), (4, 2), (5, 2), (6, 2)] -- glider
+    cellCoordinates = [ -- glider gun has takes 31 steps to be back in original state
+      (2,6),(2,7),
+      (3,6),(3,7),
+      (12,6),(12,7),(12,8),
+      (13,5),(13,9),
+      (14,4),(14,10),
+      (15,4),(15,10),
+      (16,7),
+      (17,5),(17,9),
+      (18,6),(18,7),(18,8),
+      (19,7),
+      (22,4),(22,5),(22,6),
+      (23,4),(23,5),(23,6),
+      (24,3),(24,7),
+      (26,2),(26,3),(26,7),(26,8),
+      (36,4),(36,5),
+      (37,4),(37,5)]
